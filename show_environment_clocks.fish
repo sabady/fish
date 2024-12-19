2fa47f5f62b154c5f5dae779b96c8c6998ed9654
Shany Abady <s.abady@gmail.com>
2024-12-19 11:07:14 +0200

    Update show_environment_clocks.fish

--- show_environment_clocks.fish
+++ show_environment_clocks.fish
@@ -3,19 +3,19 @@ function show_environment_clocks --description 'Show the current time of a group
       echo "Usage: show_environment_clocks <inventory file name> <inventory group name [all]>"
       return 1
   end
-  
-  cd $ANSIBLE_PATH
-  if test ! -f $argv[1]
-      echo "$argv[1] inventory does not exist"
-      return 1
-  end
-  
+
   if not set -q $argv[2]
     set group all
   else
     set group $argv[2]
   end
 
+  cd $ANSIBLE_PATH
+  if test ! -f $argv[1]
+      echo "$argv[1] inventory does not exist"
+      return 1
+  end
+  
   set hosts_count (ansible -i $argv[1] $group --list-hosts | tail -n +2 | wc -l)
   ansible -f $hosts_count -i $argv[1] $group -m raw -a 'date' | grep -v connection | grep -v Warning
   echo "There are $hosts_count hosts in the $group group"
